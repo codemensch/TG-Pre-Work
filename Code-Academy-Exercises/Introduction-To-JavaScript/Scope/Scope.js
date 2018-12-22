df84fc9f4e2b6528b6f795b06200796c87e0e9c2
Cody Fontes <codemensch@gmail.com>
2018-12-21 20:46:47 -0800

    Added comment about variable scope.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Scope.js
@@ -0,0 +1,4 @@
+/* Scope is where variables can be defined and referenced. Scope defines what
+parts of a program can "see" variables. Because of scope, some variable are
+local, can be seen by all parts of the program, others can only be seen by
+parts of the program. */
