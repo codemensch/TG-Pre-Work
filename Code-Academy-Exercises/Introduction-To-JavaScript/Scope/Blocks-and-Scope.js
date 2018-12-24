d3cf032ad6b4af96a5a5f1d7a4d27d387c98b1ad
Cody Fontes <codemensch@gmail.com>
2018-12-23 20:13:53 -0800

    Defined city variable and logCitySkyline function.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Blocks-and-Scope.js
@@ -0,0 +1,10 @@
+const city = "New York City";
+function logCitySkyline(){
+	let skyscraper = "Empire State Building";
+  return 'The stars over the ' + skyscraper + ' in ' + city;
+}
+
+console.log(logCitySkyline());
+
+/* Blocks are chunks of code set between curly braces. Blocks could be functions,
+if statements or other types of code groupings.*/
