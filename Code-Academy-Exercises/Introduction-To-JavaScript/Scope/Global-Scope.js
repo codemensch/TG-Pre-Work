3b0f33396daf0c841bd42ef9d2cb4545b42e38d4
Cody Fontes <codemensch@gmail.com>
2018-12-23 20:23:59 -0800

    Defined the callMyNightSky function which accesses global variables outside its block.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Global-Scope.js
@@ -0,0 +1,13 @@
+let satellite = 'The Moon';
+let galaxy = 'The Milky Way';
+let stars = 'North Star';
+
+function callMyNightSky(){
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+
+console.log(callMyNightSky());
+
+/*Global scope is the context in which variables are not defined inside of
+ blocks and thus are accessible to all code in the program, including code
+ inside blocks.*/
