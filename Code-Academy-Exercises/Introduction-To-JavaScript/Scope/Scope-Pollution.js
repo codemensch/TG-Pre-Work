80e9bf0af9ce6efbd7cf7cdec6b7dc413b3018df
Cody Fontes <codemensch@gmail.com>
2018-12-26 21:16:01 -0800

    Accessed global scope variable from a local scope, resulting in logging a different value from the original global variable.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Scope-Pollution.js
@@ -0,0 +1,14 @@
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+let stars = 'North Star';
+
+const callMyNightSky = () => {
+  stars = 'Sirius';
+	return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+};
+
+console.log(callMyNightSky());
+console.log(stars);
+
+/* Scope pollution is when there are too many global variables or variables are
+being used across scopes. This can cause unexpected behavior and variable values. */
