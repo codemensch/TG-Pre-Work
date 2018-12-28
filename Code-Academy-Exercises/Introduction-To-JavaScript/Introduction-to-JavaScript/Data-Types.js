872391cd3eea23bc02a1ad5377e0b76653caeb97
Cody Fontes <codemensch@gmail.com>
2018-12-27 19:59:17 -0800

    Logged strings and numbers to the console.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Data-Types.js
@@ -0,0 +1,8 @@
+console.log('JavaScript');
+console.log(2011);
+console.log('Woohoo! I love to code! #codecademy');
+console.log(20.49);
+
+/* There are seven data types in JavaScript which are the classifications
+of data one might encounter. They include: number, string, boolean, null
+undefined, symbol, and object. */
