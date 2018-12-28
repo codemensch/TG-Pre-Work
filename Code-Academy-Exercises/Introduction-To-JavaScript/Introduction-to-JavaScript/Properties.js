15a0533c1d1212354817eadf98117b343527146b
Cody Fontes <codemensch@gmail.com>
2018-12-27 20:44:05 -0800

    Logged a string's length property.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Properties.js
@@ -0,0 +1,7 @@
+console.log('Teaching the world how to code'.length);
+
+/* New data in a JavaScript program is saved as an "instance" of that data's
+data type. Each instance has different properties associated with it. A string
+instance has a property of 'length', which has stored the number of characters in
+the string. Property infornation can be retrieved by appending the property name
+to the data/string with a dot, e.g. "Hello".length. */
