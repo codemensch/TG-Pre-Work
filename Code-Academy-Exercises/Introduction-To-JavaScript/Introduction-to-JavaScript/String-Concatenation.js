fe9fb6d80465b5f113a4865de3c2ca31c7eb6b7f
Cody Fontes <codemensch@gmail.com>
2018-12-27 20:28:44 -0800

    Concatenated strings using the + operator.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/String-Concatenation.js
@@ -0,0 +1,5 @@
+console.log('Hello ' + 'World' );
+
+/* The + arithmetic operator can be used to join strings together, which is
+called concatenation. Spaces have to be explicitly within the string. Multiple
+strings ca be chained together using additional + operators.*/
