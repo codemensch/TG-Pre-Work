7d6b76526b6bc9a02c6d48fc67e4a5418b8d00d6
Cody Fontes <codemensch@gmail.com>
2018-12-27 21:03:21 -0800

    Accessed string methods by appending them to the string instance.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Methods.js
@@ -0,0 +1,10 @@
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+/*String instances also have methods associated with them like string properties.
+To access string methods, just append the method to the string instance with
+a dot operator ('hello'.toUpperCase()), Some methods take arguments, which are placed
+ between the parenthesis ('hello'.startsWith('h').)*/
