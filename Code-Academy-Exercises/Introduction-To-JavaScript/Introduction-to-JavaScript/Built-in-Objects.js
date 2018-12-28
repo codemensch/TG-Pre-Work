5ec137a4e51f8a7c0c3c69cc9208619b0a905ea6
Cody Fontes <codemensch@gmail.com>
2018-12-27 21:20:12 -0800

    Performed mathematical operations using methods in the Number and Math objects.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Built-in-Objects.js
@@ -0,0 +1,7 @@
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
+
+/* You can define your own objects but JavaScript also has some objects already 
+pre-defined. One object is the Math object that has several mathematical methods
+defined, like Math.random() which returns a random number between 0 and 1. */
