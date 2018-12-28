24f5b1a21b8afe5ec4a9e3b607b06e49b69cb36a
Cody Fontes <codemensch@gmail.com>
2018-12-27 20:14:52 -0800

    Performed arithmetic operations and logged them to the console.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Arithmetic-Operators.js
@@ -0,0 +1,10 @@
+console.log(3.5 + 35);
+console.log(2018 - 1969);
+console.log(65/240);
+console.log(.2708 * 100);
+
+/*In order to perform mathetical calculations, there are 5 arithmetic operators
+or symbols: addition (+), subtraction (-), multiplication (*), divistion (/)
+and remainder/modulo (%). All act like normal operators, except modulo returns
+the remainder of a division. The computer will evaluate an expression in a
+console.log() statement.*/
