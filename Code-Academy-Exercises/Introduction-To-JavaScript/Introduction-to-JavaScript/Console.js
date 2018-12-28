dc87fa3020c80fd7d950a7dabcda02b1a438781c
Cody Fontes <codemensch@gmail.com>
2018-12-27 18:05:08 -0800

    Printed to numbers using console.log().

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Console.js
@@ -0,0 +1,8 @@
+console.log(35);
+console.log(364);
+
+/* The console in terms of JavaScript is a window where messages and feedback
+from the computer are displayed. In the JavaScript language, the "console"
+keyword is an onject that contains data and methods, including the "console"
+object, which contains the log() function. console.log() prints input given
+to it to the console. */
