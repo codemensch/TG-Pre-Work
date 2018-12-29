16fbd7a9f9cfb8bdf9686951d1cce09ea746e0e3
Cody Fontes <codemensch@gmail.com>
2018-12-29 14:59:05 -0800

    Used bracket notation to access array element and save to variable.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Accessing-Elements.js
@@ -0,0 +1,14 @@
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+let listItem = famousSayings[0];
+
+console.log(listItem);
+
+console.log(famousSayings[2]);
+
+console.log(famousSayings[3]);
+
+/*Arrays are zero indexed, meaning the first element of the array has an index
+of 0. To access the value of a particular element by citing the name of the array
+followed by square brackets with the index (number) of the element (array[2]). This
+is bracket notation.*/
