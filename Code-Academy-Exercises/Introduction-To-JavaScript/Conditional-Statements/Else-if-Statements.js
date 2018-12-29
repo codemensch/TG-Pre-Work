a0b27ac577e190eca13f333bd00b974e401091c8
Cody Fontes <codemensch@gmail.com>
2018-12-29 09:18:42 -0800

    Added more conditional statements to the if..else statement using an else-if statement.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Else-if-Statements.js
@@ -0,0 +1,22 @@
+let season = 'summer';
+
+if (season === 'spring') {
+  console.log('It\'s spring! The trees are budding!');
+}
+else if (season === 'winter'){
+  console.log('It\'s winter! Everything is covered in snow.');
+}
+else if (season === 'fall'){
+  console.log('It\'s fall! Leaves are falling!');
+}
+else if (season === 'summer'){
+  console.log('It\'s sunny and warm because it\'s summer!');
+}
+else {
+  console.log('Invalid season.');
+}
+
+
+/*The else-if statement allows for more than two conditional statements in an
+if..else statement. The else-if statement is placed between the initial if
+statement and the ending else statement.*/
