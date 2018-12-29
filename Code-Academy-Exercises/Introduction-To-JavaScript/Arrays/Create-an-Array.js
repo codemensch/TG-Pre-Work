9991526853baaf0a27faaf45fa9814152c3ffb30
Cody Fontes <codemensch@gmail.com>
2018-12-29 14:21:49 -0800

    Created an array and saved it to a variable.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Create-an-Array.js
@@ -0,0 +1,6 @@
+const hobbies = ['hiking', 'biking', 'triking'];
+console.log(hobbies);
+
+/*An array is created by using two square brackets (array literal) [] and expanding the array
+with a list of items inside separated by commas. Arrays can be saved by assigning them
+to variables. */
