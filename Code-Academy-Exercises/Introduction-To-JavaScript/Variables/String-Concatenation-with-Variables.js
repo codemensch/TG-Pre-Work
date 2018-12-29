a9d6f1285ae56f3f214e21b518a6d45a85d48436
Cody Fontes <codemensch@gmail.com>
2018-12-28 17:26:17 -0800

    Logged a concatenated string using a variable with a string value.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/String-Concatenation-with-Variables.js
@@ -0,0 +1,7 @@
+let favoriteAnimal = 'hedgehog';
+console.log('My favorite animal: ' + favoriteAnimal);
+
+/*In string concatenation, two strings are combined with the + operator.
+When a string is stored in a variable, it can still be concatenated by chaining
+strings or variables with string values in them with the + operator. The variable
+will be interpreted as the variable's value and not the variable's name when logged.*/
