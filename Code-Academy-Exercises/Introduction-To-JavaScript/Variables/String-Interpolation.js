abb87e4d779b48faf0798d02d5ddd05fbcdb8efc
Cody Fontes <codemensch@gmail.com>
2018-12-28 17:37:02 -0800

    Inserted variable values into a string using interpolation by template literals.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/String-Interpolation.js
@@ -0,0 +1,8 @@
+let myName = 'Cody';
+let myCity = 'Charlottesville';
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+
+/*String interpolation is when the value of a variable is inserted into a string.
+To do this we can use template literals which is backticks around the string (``)
+and the variable placed in the placeholder ${myVariable}. Template literals
+are easy to read and escaping quotes isn't necessary.*/
