4405553af5f010e952d21dc078182bc8ca0ac935
Cody Fontes <codemensch@gmail.com>
2018-12-28 20:14:06 -0800

    Added logical operator to if conditional.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Logical-Operators.js
@@ -0,0 +1,14 @@
+let mood = 'sleepy';
+let tirednessLevel = 6;
+if (mood === 'sleepy' && tirednessLevel > 8){
+  console.log('time to sleep');
+}
+else{
+  console.log('not bed time yet');
+}
+
+/*To make more complex conditional statements, we can use logical operators. The three logical operators
+are and &&, or ||, and not !. && creates a condition where both statements must
+be true to evaluate to true, || is a condition where only one statement must
+evaluste to true to return true, and ! reverses the boolean value given (!true
+is false and vice versa).*/
