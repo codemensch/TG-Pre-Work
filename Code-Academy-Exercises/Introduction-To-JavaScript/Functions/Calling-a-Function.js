1e91f64d744502348b3ee9549ed389a7b83a138b
Cody Fontes <codemensch@gmail.com>
2018-12-29 10:07:49 -0800

    Called a function three times.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Calling-a-Function.js
@@ -0,0 +1,12 @@
+function sayThanks(){
+  console.log('Thank you for your purchase! We appreciate your business.');
+}
+
+sayThanks();
+sayThanks();
+sayThanks();
+
+/*Declaring a function only creates its existence but does not execute the function.
+To make the function actually run, a function has to be called. To do this,
+the function's name simply has to be evoked and can be called as many times as
+necessary.*/
