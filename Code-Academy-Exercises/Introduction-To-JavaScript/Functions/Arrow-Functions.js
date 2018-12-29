54b5dbc93505885d0043708222a883a8a820d6a1
Cody Fontes <codemensch@gmail.com>
2018-12-29 11:55:14 -0800

    Function declared using arrow function syntax.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Arrow-Functions.js
@@ -0,0 +1,10 @@
+const plantNeedsWater = (day) => {
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+/*Functions can also be defined by ommiting the function keyword and adding
+the arrow symbol => pointing to the function body () => {}*/
