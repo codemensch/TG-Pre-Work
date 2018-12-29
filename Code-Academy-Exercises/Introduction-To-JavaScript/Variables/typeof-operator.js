bc7238d5a1d94725b2943c6f78e8993b6503ad5b
Cody Fontes <codemensch@gmail.com>
2018-12-28 17:44:11 -0800

    Logged the data type of a variable before and after re-assignment with the typeof operator.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/typeof-operator.js
@@ -0,0 +1,8 @@
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+newVariable = 1;
+console.log(typeof newVariable);
+
+/*You can check the data type of a variable by placing the typeof operator next to
+the variable in question and logging the result. If you re-assign the variable to
+a different data type, the result will be reflected when logged.*/
