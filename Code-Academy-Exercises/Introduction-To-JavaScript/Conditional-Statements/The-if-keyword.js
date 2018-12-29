08d6a7d9854373b08867a1ecd29815d87bf9ca89
Cody Fontes <codemensch@gmail.com>
2018-12-28 18:03:02 -0800

    Created a condition using the if keyword.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/The-if-keyword.js
@@ -0,0 +1,8 @@
+let sale = true;
+sale = false;
+if (sale){
+  console.log('Time to buy!');
+}
+
+/*The if statement takes a condition inside parenthesis, if the condition is true,
+the code inside the curly braces will run and not if it doesn't.*/
