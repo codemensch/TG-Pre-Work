3f84f5cc81dbadb39585c607fb0583b35f7f0954
Cody Fontes <codemensch@gmail.com>
2018-12-29 10:46:37 -0800

    Default parameters defined using the assignment operator.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Default-Parameters.js
@@ -0,0 +1,9 @@
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}
+
+/*If a function is defined with a parameter and no argument is passed, a default
+argument can be defined that will pass to the function. This is done by using
+the assignment operator (=) with the parameter name, like number = 25*/
