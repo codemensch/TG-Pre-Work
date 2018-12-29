075fb1903b9a90aebf633fb12e47ca9e9a956860
Cody Fontes <codemensch@gmail.com>
2018-12-29 12:14:44 -0800

    Arrow function refactored into concise body syntax.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Concise-Body-Arrow-Functions.js
@@ -0,0 +1,7 @@
+const plantNeedsWater = day => day === 'Wednesday' ? true : false;
+
+/*Arrow functions can be further refactored into concise body arrow functions. If
+there is zero parameters, parentheses are required, if only one, none are required,
+if there are multiple they are required. Also, if the function is a single line,
+curly braces can be omitted as well as the return statement, as value from the
+function will be implicitly returned. */
