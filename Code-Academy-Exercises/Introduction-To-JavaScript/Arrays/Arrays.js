a4ec8925f8014334a1d2c6710f38cd70d2f79781
Cody Fontes <codemensch@gmail.com>
2018-12-29 14:12:57 -0800

    Added comment describing what arrays are.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Arrays.js
@@ -0,0 +1,6 @@
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+
+console.log(newYearsResolutions);
+
+/*Arrays are lists of data in JavaScript. Arrays can store any type of data
+and each item in the list is numbered by its position.*/
