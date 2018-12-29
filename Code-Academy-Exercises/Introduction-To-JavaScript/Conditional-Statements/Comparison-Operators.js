3a425c8d1fee946caece667f893eb77e29216019
Cody Fontes <codemensch@gmail.com>
2018-12-28 19:54:01 -0800

    Compared two values in an if..else statement with a comparison operator.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Comparison-Operators.js
@@ -0,0 +1,10 @@
+let hungerLevel = 7;
+if (hungerLevel > 7){
+  console.log('Time to eat!');
+}
+else {
+  console.log('We can eat later!');
+}
+
+/*In order to compare values in condition statements, we use comparison operators
+like ===, !==, <, >, <=, and >=. A comparison of two values will return true or false.*/
