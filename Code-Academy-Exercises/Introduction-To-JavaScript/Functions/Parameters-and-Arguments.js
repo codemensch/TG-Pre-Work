1841a67ce9e26eb9ef6338bafab7c29394f2efb5
Cody Fontes <codemensch@gmail.com>
2018-12-29 10:36:43 -0800

    Called a function with an argument passed through the name parameter.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Parameters-and-Arguments.js
@@ -0,0 +1,10 @@
+function sayThanks(name) {
+  console.log('Thank you for your purchase ' + name + '! We appreciate your business.');
+}
+
+sayThanks('Cole');
+
+/*Functions can also take input from outside that affect its output. A function's
+input is defined through its parameters which are placed between the parentheses.
+The actual data that is passed through these parameters are called arguments and
+are specified when the function is called.*/
