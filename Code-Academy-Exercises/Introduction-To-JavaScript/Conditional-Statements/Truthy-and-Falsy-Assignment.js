4a5f7e0a4187cf31a12c405e567729ec8bc5cc2d
Cody Fontes <codemensch@gmail.com>
2018-12-28 20:47:38 -0800

    Assigned a value to a variable based on a short-circuit evaluation using logical operators.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Truthy-and-Falsy-Assignment.js
@@ -0,0 +1,9 @@
+let tool = 'marker';
+
+// Use short circuit evaluation to assign  writingUtensil variable below:
+let writingUtensil = tool || 'pen';
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
+
+/*Because data will have a truthy or falsy value, you can do a logical comparison
+within variable assignment, this is called short circuit evaluation.*/
