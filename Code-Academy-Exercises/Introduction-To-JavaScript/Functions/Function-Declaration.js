d606920da9f55874186910162926fa1a43416942
Cody Fontes <codemensch@gmail.com>
2018-12-29 09:57:56 -0800

    Declaration of two functions using the function keyword.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Function-Declaration.js
@@ -0,0 +1,10 @@
+function getReminder(){
+  console.log('Water the plants');
+}
+
+function greetInSpanish(){
+  console.log('Buenas Tardes');
+}
+
+/*The function declaration consists of the function keyword, the function name/identifier, followed by
+parentheses, and curly braces. The curly braces are the function body.*/
