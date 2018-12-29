4fb341a721110163f6db6fa04cb6b0fe91ecb793
Cody Fontes <codemensch@gmail.com>
2018-12-28 19:42:12 -0800

    Added an else statement to the if..else statement for when the condition is false.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/if-else-Statements.js
@@ -0,0 +1,15 @@
+let sale = true;
+
+sale = false;
+
+if(sale) {
+  console.log('Time to buy!');
+}
+else {
+  console.log('Time to wait for a sale.');
+}
+
+/*If we want to execute code when the condition returns false, an else statement is
+used in conjunction with the if statement. An else statement requires an if statement.
+Like the if statement, code inside the curly braces of the else statement will run,
+but this time when the condition is false.*/
