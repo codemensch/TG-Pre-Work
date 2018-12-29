89f6e3866275b1d091a567d4bf27e012f08f6f16
Cody Fontes <codemensch@gmail.com>
2018-12-29 09:12:04 -0800

    Added a conditional statement written in ternary operator syntax.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Ternary-Operator.js
@@ -0,0 +1,15 @@
+let isLocked = false;
+
+isLocked ? console.log('You will need a key to open the door.'): console.log('You will not need a key to open the door.');
+
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!'): console.log('Incorrect!');
+
+let favoritePhrase = 'Love That!';
+
+favoritePhrase === 'Love That!' ? console.log('I love that!'): console.log("I don't love that!");
+
+/*The ternary operator is a shorthand syntax of an if..else statement. It includes
+a condition, a ?, and then two chunks of code separated by a :. The first chunck
+will execute if the condition is true and the second will execute if it is false.*/
