d89db1e1f3a23f035f14309f0de4f1d5db285052
Cody Fontes <codemensch@gmail.com>
2018-12-28 16:57:15 -0800

    Updated let keyword comment.

--- Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable:let.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable:let.js
@@ -3,5 +3,6 @@ changeMe = false;
 console.log(changeMe);
 
 /*let is another keyword for declaring a variable. When using the let keyword,
-the variable can be reassigned a new value. Variables can be declared without
-assigning a value to them and by default will have 'undefined' as their value.*/
+the variable can be reassigned a new value. Variables declared with let can be
+declared without assigning a value to them and by default will have 'undefined'
+as their value.*/
