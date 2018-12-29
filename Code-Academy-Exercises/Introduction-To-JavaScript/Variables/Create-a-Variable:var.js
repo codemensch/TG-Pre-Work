5af5cf6bc7117866450094b2c53e06139e541440
Cody Fontes <codemensch@gmail.com>
2018-12-28 16:47:42 -0800

    Declared variables using the var keyword.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable:var.js
@@ -0,0 +1,9 @@
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
+console.log(favoriteFood);
+console.log(numOfSlices);
+
+/*The original keyword for assigning a variable is 'var'. To assign a variable
+using var one would write var, the name of the variable, the assignment operator
+(=), and then a value (var myVariable = 6). Variable names must follow JavaScript
+naming conventions.*/
