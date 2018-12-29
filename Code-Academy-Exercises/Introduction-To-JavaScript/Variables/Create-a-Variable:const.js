af26430e301ab8b8e6a1af8c24d1c40d775f9e70
Cody Fontes <codemensch@gmail.com>
2018-12-28 17:01:13 -0800

    Declared a variable with the const keyword.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable:const.js
@@ -0,0 +1,6 @@
+const entree = 'Enchiladas';
+console.log(entree);
+entree = 'Tacos';
+
+/*the const keyword declares a variable but the variable must have a value and
+the value can not be reassigned.*/
