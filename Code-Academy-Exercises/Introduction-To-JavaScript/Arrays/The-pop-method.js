f1810d175bd7e3cd9deae9c5a73aba60d79d8f0d
Cody Fontes <codemensch@gmail.com>
2018-12-29 17:00:41 -0800

    Removed the last element of an array with pop().

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/The-pop-method.js
@@ -0,0 +1,7 @@
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+
+chores.pop();
+console.log(chores);
+
+/*To remove an element from the end of the array, you can use the pop() method
+using dot notation. pop() does not take any arguments, just removes the last element.*/
