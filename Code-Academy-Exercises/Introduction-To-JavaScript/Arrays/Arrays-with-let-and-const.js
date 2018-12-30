c09c76bd233c8ffadb393e1ee692dfdd5c0edecf
Cody Fontes <codemensch@gmail.com>
2018-12-29 16:47:23 -0800

    Declared arrays with let and const.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Arrays-with-let-and-const.js
@@ -0,0 +1,16 @@
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = 'Mayo';
+console.log(condiments);
+
+condiments = ['Mayo'];
+console.log(condiments);
+
+utensils[3] = 'Spoon';
+console.log(utensils);
+
+/*Arrays can be declared with let and const. Unlike variables, when declared with
+const, the array can not be replaced by a new array or value, but the elements of the
+array can be changed.*/
