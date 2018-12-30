c21c752122a1f7905e16e86cd1ebc9e958d09aec
Cody Fontes <codemensch@gmail.com>
2018-12-29 21:19:10 -0800

    Logged all elements of an array to the console manually.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Repeating-Tasks-Manually.js
@@ -0,0 +1,8 @@
+// Write your code below
+let vacationSpots = ['Paris', 'Brussels', 'Brighton'];
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+/*Loops make it possible to not have to do repetitive, incremental tasks manually, like
+logging all of the elements of a large array like above.*/
