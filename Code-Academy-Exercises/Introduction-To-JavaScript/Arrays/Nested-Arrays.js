a26320df1833cdf0159c18eae4805beaf7be2368
Cody Fontes <codemensch@gmail.com>
2018-12-29 17:47:54 -0800

    Saved the value of an element in a nested array to a variable.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Nested-Arrays.js
@@ -0,0 +1,7 @@
+let numberClusters = [[1, 2], [3, 4], [5, 6]];
+const target = numberClusters[2][1];
+
+/*Nested arrays are arrays in array elements. To access a specific element in a
+nested array, we would use bracket notation, the first bracket representing the
+index of the nested array, and the second representing the index of the specific
+element of the nested array we want to access as seen above.*/
