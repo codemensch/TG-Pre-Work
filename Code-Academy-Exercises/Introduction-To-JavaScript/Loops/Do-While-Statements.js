c4e586ae668162dc753378d0a7b09dee2e2ba672
Cody Fontes <codemensch@gmail.com>
2018-12-30 15:23:39 -0800

    Incremented a variable using a do..while loop.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Do-While-Statements.js
@@ -0,0 +1,14 @@
+// Write your code below
+let cupsOfSugarNeeded = 2;
+let cupsAdded = 0;
+
+do{
+  cupsAdded++;
+}
+while(cupsAdded < cupsOfSugarNeeded);
+
+/*If it is required that code in the loop needs to run at least once, we can
+use the do..while loop. The do in the do..while loop takes the loop code in
+curly braces and will immediately run once. The while in the do..while is after
+the do statement and contains the stopping condition, continuing to run the
+do statement until the condition is no longer true.*/
