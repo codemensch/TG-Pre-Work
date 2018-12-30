84e7814c9a5d680551ffff334c125c62aad022be
Cody Fontes <codemensch@gmail.com>
2018-12-30 08:49:37 -0800

    Logged each element of an array with a for loop.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Looping-through-Arrays.js
@@ -0,0 +1,12 @@
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+// Write your code below
+for(let i=0; i<vacationSpots.length; i++){
+  console.log(vacationSpots[i]);
+}
+
+/*To loop through an array we first modify the stopping condition by making the
+maximum number the length of the array. The easiest way to find the length of
+an array is to use .length. Then we use bracket notation to reference each element
+in the array by replacing the index number with our iterat variable, which will
+update with every turn of the for loop.*/
