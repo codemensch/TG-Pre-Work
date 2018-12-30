766725e5071d108fa37390133776750b9c460849
Cody Fontes <codemensch@gmail.com>
2018-12-30 08:35:29 -0800

    Added a for loop.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/The-For-Loop.js
@@ -0,0 +1,10 @@
+// Write your code below
+for(let i=5; i<11; i++){
+  console.log(i);
+}
+
+
+/*One type of loop is the for loop. The for loop has three parts: an initialization
+statement where a variable is initialized, a stopping condition where the variable is checked,
+and an iteration statement where the variable is updated if the stopping condition hasn't
+been met. When the stopping condition has been met the loop stops.*/
