aabbac18f0cc735b5b3f22fcb432ddf44bfc4285
Cody Fontes <codemensch@gmail.com>
2018-12-29 21:00:20 -0800

    Added a comment describing what a loop in programming is.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Loops.js
@@ -0,0 +1,4 @@
+/*Loops are a construct often used in programming where a set of code is repeated
+over and over until a condition is met, at which case the loop stops and
+the next part of the program continues. A loop repitition is also known as an
+iteration.*/
