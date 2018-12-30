32cacd8c6c32c8dec4431d6319fc4855921d3f09
Cody Fontes <codemensch@gmail.com>
2018-12-29 16:39:38 -0800

    Updated the second element of the array with bracket notation and the assignment operator.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Update-Elements.js
@@ -0,0 +1,5 @@
+let groceryList = ['bread', 'tomatoes', 'milk'];
+groceryList[1] = 'avocados';
+
+/*To update a specific element of an array, simply access the element with
+bracket notation and assign the new value using the assignment operator as above.*/
