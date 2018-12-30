ac2ac9ba54372eadce0fff9b97c25a9667f44a07
Cody Fontes <codemensch@gmail.com>
2018-12-29 16:57:48 -0800

    Added elements to the array using the push() method and dot notation.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/The-push-method.js
@@ -0,0 +1,9 @@
+const chores = ['wash dishes', 'do laundry', 'take out trash'];
+
+chores.push('make bed', 'sweep chimney');
+console.log(chores);
+
+/*Arrays have built in methods as well. One method is the push() method that adds an
+element to the end of the array. To use the method, use dot notation to chain the
+method to the array. In the parentheses, list the elements to add to the array
+and separate them with commas as seen above.*/
