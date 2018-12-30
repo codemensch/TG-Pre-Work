3c4bed3c90c7010ffd489421debaae5d2ea29454
Cody Fontes <codemensch@gmail.com>
2018-12-29 16:51:51 -0800

    Logged the length of the array using dot notation.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/The-length-property.js
@@ -0,0 +1,6 @@
+const objectives = ['Learn a new languages', 'Read 52 books', 'Run a marathon'];
+console.log(objectives.length);
+
+/*Length is a built-in property of arrays. The value of an array's length property
+can be accessed by chaining ".length" to the array and logging the result to
+the console or saving to a variable as seen above. This is called dot notation.*/
