9542184cceeb6fd4598220dac3919fe203df3e60
Cody Fontes <codemensch@gmail.com>
2018-12-30 16:54:52 -0800

    Added comment describing abstraction and higher order functions.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Higher-Order-Functions/Introduction.js
@@ -0,0 +1,6 @@
+/*In programming we want to be able to communicate more precisely and more easily
+concepts and elements of a program. This can be difficult to do if we have to detail
+every part of a program or debug code line by line. By writing functions we can abstract
+or make parts of a program modular or more logical. Another way of establishing
+abstraction or modular code is to use higher order functions, which take or return
+functions as arguments or output instead of passing small bits of data.*/
