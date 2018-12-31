51a6e870c64ba16609c88c2fd0f8c2405ab7dffd
Cody Fontes <codemensch@gmail.com>
2018-12-30 20:46:43 -0800

    Used .forEach to manipulate each array element.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Iterators/The-forEach-Method.js
@@ -0,0 +1,8 @@
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+fruits.forEach( fruit => console.log(`I want to eat a ${fruit}`));
+
+/*The .forEach iterator iterates over each array element and executes a callback
+function passed as an argument on each array element. Any of the syntaxes for functions
+can be used in defining the callback function.*/
