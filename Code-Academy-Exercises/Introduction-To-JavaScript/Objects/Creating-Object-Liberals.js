f6c559bc45917a4ff76d814c29ce19e4fcf7070e
Cody Fontes <codemensch@gmail.com>
2019-01-01 17:08:09 -0800

    Added the fasterShip object with properties.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Creating-Object-Liberals.js
@@ -0,0 +1,9 @@
+// Write your fasterShip object literal below
+let fasterShip = {
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel'
+}
+
+/*Objects can be assigned to variables just like other data types. An object literal
+is designated using curly braces {}. An object can have properties which are made
+up of key-value pairs that are separated by commas.*/
