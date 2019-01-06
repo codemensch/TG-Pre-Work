10819abb8baf1bb16608b40a728cad912e8eb12f
Cody Fontes <codemensch@gmail.com>
2019-01-05 17:53:12 -0800

    Defining the constructor() of a class.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Constructor.js
@@ -0,0 +1,10 @@
+class Surgeon {
+  constructor(name, department) {
+    this._name = name;
+    this._department = department;
+  }
+}
+
+/*The major difference between a class object and a normal object is the constructor.
+The constructor method is called by JavaScript every time a new instance of a class is made.
+constructor() sets the values of the class instance to the value of the arguments.*/
