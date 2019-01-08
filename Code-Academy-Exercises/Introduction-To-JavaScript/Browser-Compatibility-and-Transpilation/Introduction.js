c8411edfcadb68b7fa91cbc84ae4d021e5f43d1e
Cody Fontes <codemensch@gmail.com>
2019-01-07 21:16:01 -0800

    Added comment describing browser compatibility issues.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/Introduction.js
@@ -0,0 +1,14 @@
+var pasta = "Spaghetti"; // ES5 syntax
+
+const meat = "Pancetta"; // ES6 syntax
+
+let sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
+
+/*When JavaScript, or any other web language, is updated or undergoes syntax changes
+there is a period of time when web browsers are incompatible with some of the
+syntax changes and these changes may present security vulverabilities. Part of being
+a developer is learning to deal with language updates. Two tools in helping with this is
+caniuse.com and Babel. */
