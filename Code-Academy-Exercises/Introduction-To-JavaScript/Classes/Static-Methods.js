40598dbe7e1872e10ae73d661934db620529c4ff
Cody Fontes <codemensch@gmail.com>
2019-01-07 18:12:48 -0800

    Added static method to HospitalEmployee.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Static-Methods.js
@@ -0,0 +1,49 @@
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+
+  static generatePassword(){
+    return Math.floor(Math.random()* Math.floor(10000));
+  }
+}
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._certifications = certifications;
+  }
+
+  get certifications() {
+    return this._certifications;
+  }
+
+  addCertification(newCertification) {
+    this.certifications.push(newCertification);
+  }
+}
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+nurseOlynyk.takeVacationDays(5);
+console.log(nurseOlynyk.remainingVacationDays);
+nurseOlynyk.addCertification('Genetics');
+console.log(nurseOlynyk.certifications);
+
+/*We can also create methods that are simply methods that belong to the actual class
+and not the instances of classes. These are called static methods and are created
+by invoking the static keyword before a method declaration in the main body of the
+class. The static class can only be called by referencing the class name and not any
+instances of a class.*/
