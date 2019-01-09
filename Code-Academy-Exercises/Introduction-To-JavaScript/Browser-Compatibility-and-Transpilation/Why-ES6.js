e94af643a5bd29e9b07869c9b03167c930e84605
Cody Fontes <codemensch@gmail.com>
2019-01-08 16:37:20 -0800

    Reverting to ES5 syntax with var.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/Why-ES6.js
@@ -0,0 +1,15 @@
+var pasta = "Spaghetti"; // ES5 syntax
+
+var meat = "Pancetta"; // ES6 syntax
+
+var sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+var carbonara = 'You can make carbonara with ' + pasta + ', ' + meat + ', and a sauce made with ' + sauce.;
+
+/*The ES6 update for JavaScript made the language more like other object oriented
+languages, thus attracting more developers to use it. It also requires less code to
+achieve the same functionality (economy of code). The ES6 update also addresses bug
+issues encountered in ES5. Since the changes were adopted early before browsers
+could establish compatibility, ES6 was made backward compatible. This means that new
+syntax can be mapped to old (ES5) syntax.*/
