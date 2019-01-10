72d9e4c8beb8a59fe5dd89f086a7331c4d89e9b6
Cody Fontes <codemensch@gmail.com>
2019-01-09 17:50:37 -0800

    Defining JavaScript modules.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/Hello-Modules.js
@@ -0,0 +1,3 @@
+/*JavaScript modules are pieces of code with similar logic that can be reused and
+imported from one program into other programs. Modules help with debugging,
+reusing code, information security, and preventing naming conflicts.*/
