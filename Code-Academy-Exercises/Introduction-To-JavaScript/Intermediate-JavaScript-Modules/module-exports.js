16c7f59e5059ddcd3b21b8cc5ee946a3c4f9a5a5
Cody Fontes <codemensch@gmail.com>
2019-01-09 18:13:06 -0800

    Creating and exporting a module.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/module-exports.js
@@ -0,0 +1,8 @@
+let Airplane = {};
+Airplane.myAirplane = 'StarJet';
+module.exports = Airplane;
+
+/*Exporting a module requires creating an object that represents the module and using
+module.exports to export the module. Properties can be added to the module by calling the
+module and the new property name in dot notation and assigning a value
+(Object.newProperty = value).*/
