2a156602ad1465fa93fe34153476d0880d70de9f
Cody Fontes <codemensch@gmail.com>
2019-01-12 10:24:45 -0800

    Exporting a module with export default.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/export-default.js
@@ -0,0 +1,8 @@
+let Airplane = {};
+
+Airplane.availableAirplanes = [{name: 'AeroJet', fuelCapacity: 800}, {name: 'SkyJet', fuelCapacity: 500}];
+
+export default Airplane;
+
+/*The ES6 syntax for exporting a single module is export default instead of
+module.exports. */
