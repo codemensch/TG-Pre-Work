dfb7b26dc52172f15c7614973bb04379cca69033
Cody Fontes <codemensch@gmail.com>
2019-01-12 14:16:48 -0800

    Importing objects/models using the import keyword.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/import.js
@@ -0,0 +1,12 @@
+import Airplane from './airplane';
+
+function displayFuelCapacity() {
+  Airplane.availableAirplanes.forEach(function(element){
+  console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+  });
+}
+
+displayFuelCapacity();
+
+/*We can also use the new import keyword to import modules/objects. The syntax is
+import ObjectModuleName from 'url'. */
