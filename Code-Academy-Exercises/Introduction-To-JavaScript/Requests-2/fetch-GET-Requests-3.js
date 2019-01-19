826130d8e51d0bb3a24fc41e867286c3c6099e72
Cody Fontes <codemensch@gmail.com>
2019-01-19 15:26:07 -0800

    Searching datamuse API with a query string parameter.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/fetch-GET-Requests-3.js
@@ -0,0 +1,38 @@
+// Information to reach API
+const url = 'https://api.datamuse.com/words';
+const queryParams = '?sl=';
+
+// Selects page elements
+const inputField = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
+
+// AJAX function
+const getSuggestions = () => {
+  const wordQuery = inputField.value;
+  const endpoint = url + queryParams + wordQuery;
+  fetch(endpoint).then(response => {
+    if(response.ok){
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  }, networkError => {
+    console.log(networkError.message);
+  });
+}
+
+// Clears previous results and display results to webpage
+const displaySuggestions = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  getSuggestions();
+};
+
+submit.addEventListener('click', displaySuggestions);
+
+/*In our getSuggestions function, we are concatenating the url of the endpoint, the
+query parameter, which narrows down the search results, and the word inputed by the user
+used to narrow the search. This then is used by the fetch() and then() methods to
+check for responses and errors and return the response.*/
