4579694ac2e4ea76a6119c64ab0d4039c1b9495b
Cody Fontes <codemensch@gmail.com>
2019-01-19 13:45:49 -0800

    Description of a fetch() GET request.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/fetch-GET-Requests-1.js
@@ -0,0 +1,3 @@
+/*One type of GET request is fetch(). fetch() sends a request to a specified endpoint,
+receives a response object and converts the object to json. If the request fails, fetch()
+will handle errors as well as execute necessary code if the request is successful.*/
